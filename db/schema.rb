a75fc74e1cab34b64038fd190cfa5957ed911214
Guillaume Delente <guillaume.delente+github@gmail.com>
2014-05-14 18:09:02 -0700

    Adds User has_many applications

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140508021302) do
+ActiveRecord::Schema.define(version: 20140515010710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: true do |t|
+    t.string   "name"
+    t.string   "package"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
